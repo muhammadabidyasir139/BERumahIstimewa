1987f81eaa954db660b57f2265c2aa86d5cd4a29
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-12-26 21:55:11 +0700

    fix s3 bucket name for update v2

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,18 +1,26 @@
 const AWS = require("aws-sdk");
 
+// Debug logging for environment variables
+console.log("S3 Config - AWS_S3_ENDPOINT:", process.env.AWS_S3_ENDPOINT);
+console.log("S3 Config - AWS_S3_BUCKET_NAME:", process.env.AWS_S3_BUCKET_NAME);
+
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION || "us-east-1",
 });
 
+const endpoint = process.env.AWS_S3_ENDPOINT || "https://is3.cloudhost.id";
+console.log("S3 Config - Using endpoint:", endpoint);
+
 const s3 = new AWS.S3({
-  endpoint: process.env.AWS_S3_ENDPOINT || "https://is3.cloudhost.id",
+  endpoint: endpoint,
   s3BucketEndpoint: true,
   s3ForcePathStyle: true,
   signatureVersion: "v4",
 });
 
 const bucketName = process.env.AWS_S3_BUCKET_NAME || "rumahistimewa";
+console.log("S3 Config - Using bucket:", bucketName);
 
 module.exports = { s3, bucketName };
